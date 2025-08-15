6b267b0d089ed593734852c17d1ef11e8681e28f
Mohamed Nheri <nherimohamed@gmail.com>
2025-08-15 17:28:22 -0400

    Refactors hardcoded components to use dynamic arrays
    Replaces repetitive hardcoded Post and Button components with data-driven rendering using map functions.
    
    Improves code maintainability and reduces duplication by centralizing component data in arrays and generating components dynamically.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,25 @@ import Post from './components/postComponent/post.jsx';
 import Buttons from './components/buttonComponent/buttons.jsx';
 import Container from './components/container/container.jsx';
 
+const post =[
+  { postTitle: "Post Title 1", postContent: "Post Content 1" },
+  { postTitle: "Post Title 2", postContent: "Post Content 2" },
+  { postTitle: "Post Title 3", postContent: "Post Content 3" },
+  { postTitle: "Post Title 4", postContent: "Post Content 4" },
+  { postTitle: "Post Title 5", postContent: "Post Content 5" },
+  { postTitle: "Post Title 6", postContent: "Post Content 6" },
+];
+const posts = post.map((p, index) => {
+  return <Post key={index} postTitle={p.postTitle} postContent={p.postContent} />;
+});
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Container />
-      <Post postTitle="Post Title 1" postContent="Post Content 1" />
-      <Post postTitle="Post Title 2" postContent="Post Content 2" />
-      <Post postTitle="Post Title 3" postContent="Post Content 3" />
-      <Post postTitle="Post Title 4" postContent="Post Content 4" />
-      <Post postTitle="Post Title 5" postContent="Post Content 5" />
-      <Post postTitle="Post Title 6" postContent="Post Content 6" />
+      {posts}
+
     </div>
   );
 }
--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -3,31 +3,24 @@
 import "./container.css";
 import Buttons from "../buttonComponent/buttons";
 
+const buttons = [
+  { postTitle: "Submit", postContent: "*****" },
+  { postTitle: "Cancel", postContent: "&&&&&" },
+  { postTitle: "Erase", postContent: "#####" },
+  { postTitle: "Click Me", postContent: "!!!!!!!" },
+];
+
+const buttonComponents = buttons.map((button, index) => (
+  <Buttons key={index} postTitle={button.postTitle}>
+    <div className="post-content">{button.postContent}</div>
+  </Buttons>
+));
+
 function Container() {
   return (
     <div className="tag-buttons-container">
       <div className="tag-buttons-grid">
-        <Buttons postTitle = "Submit">
-            <div className="post-content">
-                *****
-            </div>
-        </Buttons>
-        <Buttons postTitle = "Cancel">
-            <div className="post-content">
-                &&&&&
-            </div>
-        </Buttons>
-        <Buttons postTitle = "Erase">
-            <div className="post-content">
-                #####
-            </div>
-        </Buttons>
-        <Buttons postTitle = "Click Me">
-            <div className="post-content">
-                !!!!!!
-            </div>
-        </Buttons>
-
+        {buttonComponents}
       </div>
     </div>
   );
